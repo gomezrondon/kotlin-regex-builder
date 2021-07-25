430619a76e48afc5d8b6733a9ecf7cc1442c2c4a
gomezrondon <gomezrondon@gmail.com>
2021-07-25 11:15:57 -0600

    test training data for spacy NER

--- src/test/kotlin/Test2.kt
+++ src/test/kotlin/Test2.kt
@@ -1,16 +1,4 @@
 import com.gomezrondon.RegEx
-import com.gomezrondon.RegEx.static.LETTERS_LOWER_CASE
-import com.gomezrondon.RegEx.static.LETTERS_UPPER_CASE
-import com.gomezrondon.RegEx.static.addToRange
-import com.gomezrondon.RegEx.static.anyLetter
-import com.gomezrondon.RegEx.static.digit
-import com.gomezrondon.RegEx.static.group
-import com.gomezrondon.RegEx.static.letter
-import com.gomezrondon.RegEx.static.literal
-import com.gomezrondon.RegEx.static.range
-import com.gomezrondon.RegEx.static.setBetween
-import com.gomezrondon.RegEx.static.wordBoundary
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test;
 
@@ -49,44 +37,75 @@ class Test2 {
 
 
     @Test
+    fun getIndexForNER() { // (52, 58, "PLUGVERSION"), (61, 97, "PLUGID"),  (0, 7,"PLUGIN"),(10, 48,"PLUGID"),(49, 75,"PLUGVERSION")
+       // val term =" I ordered this from ShopClues" //length = 29
+//         val term =" Who is Shaka Khan?" //length = 29 //(7, 17, "PERSON") (0, 7, "PERSON")
+//        val term ="_plugins { id 'org.jetbrains.kotlin.jvm' version '1.4.21' id 'io.spring.dependency-management' version '1.0.10.RELEASE'  } " //length = 29 //(7, 17, "PERSON")
+        val line ="plugins { id ' org.jetbrains.kotlin.jvm ' version ' 1.4.21 ' id ' io.spring.dependency-management ' version ' 1.0.10.RELEASE '  }" //length = 29 //(7, 17, "PERSON")
+
+        val term = listOf<String>("plugins","id ' org.jetbrains.kotlin.jvm '","version ' 1.4.21 '","id ' io.spring.dependency-management '","version ' 1.0.10.RELEASE '")
+//(0, 7,"PLUGIN"),(10, 41,"PLUGID"),(42, 60,"PLUGVERSION"),(61, 99,"PLUGID"),(100, 126,"PLUGVERSION")
+        term.forEach {
+            val indexOf = line.indexOf(it)
+            if (indexOf >= 0) {
+                val ends = indexOf + it.length
+                println(it)
+                println(""",($indexOf, $ends,"X")""")
+            }
+
+        }
+
+//        term.mapIndexed { index, c -> println("i: $index letter: $c") }
+
+        println("length: ${line.length}")
+    }
+
+    @Test
     fun findRemoveSpaces() {
         val temStr ="""
-            plugins {
-                id 'org.jetbrains.kotlin.jvm' version '1.5.20'
-                id 'application'
-            }
+plugins {
+    id 'org.jetbrains.kotlin.jvm' version '1.5.20'
+    id 'application'
+    id 'org.springframework.boot' version '2.5.2'
+}
 
-            group = 'com.gomezrondon'
-            version = '1.0-SNAPSHOT'
+plugins {
+    id 'org.jetbrains.kotlin.jvm' version '1.4.0'
+}
 
-            repositories {
-                mavenCentral()
-                maven {
-                    url 'https://alphacephei.com/maven/'
-                }
-            }
-            dependencies {
-                implementation group: 'net.java.dev.jna', name: 'jna', version: '5.7.0'
-                implementation group: 'com.alphacephei', name: 'vosk', version: '0.3.30+'
-                implementation group: 'org.jetbrains.kotlinx', name: 'kotlinx-coroutines-core', version: '1.3.5'
+plugins {
+    id 'org.springframework.boot' version '2.4.3'
+    id 'io.spring.dependency-management' version '1.0.11.RELEASE'
+    id 'java'
+    id 'org.jetbrains.kotlin.jvm' version '1.4.30'
+}
 
-            }
-            
-            test {
-                useJUnitPlatform()
-            }
+plugins {
+	id 'org.springframework.boot' version '2.5.0'
+	id 'io.spring.dependency-management' version '1.0.11.RELEASE'
+	id 'java'
+	id 'org.springframework.experimental.aot' version '0.10.0-SNAPSHOT'
+}
 
+plugins {
+    id 'java'
+}
 
-            jar {
-                manifest {
-                    attributes 'Main-Class': 'com.gomezrondon.RegEx'
-                }
+plugins {
+	id 'org.springframework.boot' version '2.5.0-SNAPSHOT'
+	id 'io.spring.dependency-management' version '1.0.10.RELEASE'
+	id 'java'
+	id 'org.jetbrains.kotlin.jvm' version '1.4.21'
+}
 
-                // This line of code recursively collects and copies all of a project's files
-                // and adds them to the JAR itself. One can extend this task, to skip certain
-                // files or particular types at will
-                from { configurations.compile.collect { it.isDirectory() ? it : zipTree(it) } }
-            }
+plugins {
+    id 'java'
+    id 'io.quarkus'
+}
+
+plugins {
+    id 'java'
+}
         """.trimIndent()
 
         val buildRegExp = RegEx()
